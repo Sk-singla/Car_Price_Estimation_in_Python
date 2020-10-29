ae3cda7630d03182386734ba3a3ff08d9d4ef03a
SuperSk <samarashu02@gmail.com>
2020-10-29 23:11:26 +0530

    Code improvement and 15 year policy added

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import datetime
 import pandas as pd
 from sklearn import linear_model
 
@@ -28,17 +29,15 @@ for i in range(train_data.shape[0]):
     train_data.at[i, 'Engine(CC)'] = train_data['Engine'][i].split()[0]
     train_data.at[i, 'Power(bhp)'] = train_data['Power'][i].split()[0]
 
-# Converting values of Mileage and Engine to float
+# Converting values to float
 train_data['Mileage(km/kg)'] = train_data['Mileage(km/kg)'].astype(float)
 train_data['Engine(CC)'] = train_data['Engine(CC)'].astype(float)
-
 
 
 count = 0
 position = []
 for i in range(train_data.shape[0]):
     if train_data['Power(bhp)'][i]=='null':
-        x = 'Y'
         count = count + 1
         position.append(i)
 
@@ -54,6 +53,8 @@ for i in range(train_data.shape[0]):
         
 train_data['New_car_Price'] = train_data['New_car_Price'].astype(float)
 
+
+# Droping the columns which are not needed
 train_data.drop(["Name"],axis=1,inplace=True)
 train_data.drop(["Mileage"],axis=1,inplace=True)
 train_data.drop(["Engine"],axis=1,inplace=True)
@@ -61,8 +62,8 @@ train_data.drop(["Power"],axis=1,inplace=True)
 train_data.drop(["New_Price"],axis=1,inplace=True)
 
 
+# Owner type - string to number
 train_data.replace({"First":1,"Second":2,"Third": 3,"Fourth & Above":4},inplace=True)
-
 
 train_data.drop(["Company"],axis=1,inplace=True)
 
@@ -80,8 +81,12 @@ var = 'Transmission'
 Transmission = train_data[[var]]
 Transmission = pd.get_dummies(Transmission,drop_first=True)
 
+
+# Final data -
 final_train= pd.concat([train_data,Location,Fuel_t,Transmission],axis=1)
 
+
+# Linear Regression - y= mx+c
 X = final_train.loc[:,['Year', 'Kilometers_Driven', 'Owner_Type', 'Seats',
        'Mileage(km/kg)', 'Engine(CC)', 'Power(bhp)', 
        'Location_Bangalore', 'Location_Chennai', 'Location_Coimbatore',
@@ -92,74 +97,96 @@ X = final_train.loc[:,['Year', 'Kilometers_Driven', 'Owner_Type', 'Seats',
 
 y = final_train.loc[:,['Price']]
 
-
-# Linear Regression
-model = linear_model.LinearRegression();
+model = linear_model.LinearRegression()
 model.fit(X,y)
 
-coef = model.coef_;
-intercept = model.intercept_;
-coefficient = [];
+coef = model.coef_             # coefficients(m)
+intercept = model.intercept_   # intercept(c)
+
+
+# getting coeffiecients in on dimension
+coefficient = []
 for i in coef[0]:
     coefficient.append(i)
     
-    
+
+# Getting All Locations and All Fuel types in dataset to show in gui
 all_Locations = [i[9:] for i in Location.columns]
 all_fuel = [i[10:] for i in Fuel_t.columns]
 all_fuel = tuple(all_fuel)
 all_Locations = tuple(all_Locations)
 
 
+
 # ==========Functions to calculate===================
 
 def estimate():
+	# if car is older than 15 years(can't sell) or year is more than current year
+    curYear = datetime.datetime.now().year
+    age = curYear-yearVar.get()
+    if(age > 15 or age<0 ):
+        return 0;
+    
+    # Getting Location of car to give that location coefficient to price
     loc = city.get()
     cityTotal =0
-    
     for i in range(len(all_Locations)):
         if all_Locations[i] == loc:
             cityTotal = coefficient[i+7]
+            break
     
+    # Getting Fuel Type of car to give that location coefficient to price
     fuel_type = Fuel.get()
     fuel_total = 0
     for i in range(len(all_fuel)):
         if all_fuel[i] == fuel_type:
             fuel_total = coefficient[i+17]
+            break
     
-    y = (yearVar.get()*coefficient[0]+kmVar.get()*coefficient[1] + (owner.current() +1)*coefficient[2] + seatVar.get()*coefficient[3] + engineVar.get()*coefficient[5] + mileageVar.get()*coefficient[4] +powerVar.get()*coefficient[6] +
-     cityTotal + fuel_total+
-     trans.current()*coefficient[20] + intercept[0]
-    )
+    # Price Calculation (y = m1x1 + m2x2 + ... + c)
+    y = ( yearVar.get()*coefficient[0]+kmVar.get()*coefficient[1] + (owner.current() +1)*coefficient[2] + seatVar.get()*coefficient[3] + 
+    	engineVar.get()*coefficient[5] + mileageVar.get()*coefficient[4] +powerVar.get()*coefficient[6] + cityTotal + fuel_total+
+    	trans.current()*coefficient[20] + intercept[0] )
     return y
 
 
-
-def goBack():
-    frame2.place_forget()
-    frame1.place(x=0,y=0,width=530,height = 500)
-    
+  
 def prediction(l):
+	# Some compulsory values, if not give then show error
     if(yearVar.get()==0 or seatVar.get()==0 or Fuel.current()==-1 or trans.current()== -1 or city.current()==-1 or owner.current==-1):
         messagebox.showerror("Error", "Values are missing")
         return
     
+
     frame1.place_forget()
+    frame2 = Frame(win,bg='#557bc2')
     frame2.place(x=0,y=0,width=530,height = 500)
     
+    # Back Button Function for frame2
+    def goBack():
+        frame2.place_forget()
+        frame1.place(x=0,y=0,width=530,height = 500)
     
+    # Calling estimate to get price
     price = estimate()
-    price = '%.4f'%price
+    output = "price"
+
+    if(price == 0):
+        output = "Your Car is Not under 15 Year Policy!!"
+    elif(price < 0):
+    	output = "Your Car is Not in condition to sell!!"
+    else:
+        price = '%.3f'%price
+        output = f"Estimated Price is Rs. {price} Lakh"
     
-    output = f"Estimated Price is Rs. {price} Lakh"
-    
-    myfont2 = font.Font(size=15)
+    myfont2 = font.Font(size=15) # Font property
     tot = Label(frame2,text = output,fg='green',font=myfont2)
     tot.place(x=100,y=100)
     
     back = Button(frame2,text = "Back",font=myfont2,command = goBack)
-    back.place(x=200,y=180)
+    back.place(x=225,y=180)
     
-    
+     
     
 # =======Tkinter Part========================
 
@@ -172,7 +199,7 @@ myfont = font.Font(size=10)
 frame1 = Frame(win,bg='#557bc2')
 frame1.place(x=0,y=0,width=530,height=500)
 
-frame2 = Frame(win,bg='#557bc2')
+
 
 # YEAR 
 yearVar = IntVar()
@@ -256,5 +283,5 @@ l = [yearVar,kmVar,mileageVar,seatVar,engineVar,powerVar]
 
 # Predict Button
 predict = Button(frame1,text='PREDICT' , command=lambda: prediction(l))
-predict.place(x=200,y=280)
+predict.place(x=225,y=260)
 win.mainloop()
